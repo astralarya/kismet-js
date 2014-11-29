b00ebb912e45f4b3b2978c5e64f23f295513922c
Mara Kim <hacker.root@gmail.com>
2014-11-29 17:45:21 -0600

    Add whitespace test

--- test/index.js
+++ test/index.js
@@ -2,6 +2,9 @@ var should = require('chai').should(),
     kismet = require('../index');
 
 describe('#parse', function() {
+    it('ignores whitespace', function() {
+      kismet.parse('\t2  +\t\t 3').should.equal(5);
+    });
     it('performs arithmetic', function() {
       kismet.parse('2+2').should.equal(4);
       kismet.parse('5-3').should.equal(2);
@@ -11,5 +14,6 @@ describe('#parse', function() {
       kismet.parse('(2+3)*4+5').should.equal(25);
       kismet.parse('4^3').should.equal(64);
       kismet.parse('16^0.5').should.equal(4);
+      kismet.parse('1.23e-10*100').should.equal(1.23e-8);
     });
 });
