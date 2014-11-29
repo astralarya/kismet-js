971a113f9dcdd2a6b496ea1dbeebb9ec3a60c393
Mara Kim <hacker.root@gmail.com>
2014-11-29 16:23:38 -0600

    Add more arithmetic tests

--- test/index.js
+++ test/index.js
@@ -2,7 +2,13 @@ var should = require('chai').should(),
     kismet = require('../index');
 
 describe('#parse', function() {
-    it('performs addition', function() {
+    it('performs arithmetic', function() {
       kismet.parse('2+2').should.equal(4);
+      kismet.parse('5-3').should.equal(2);
+      kismet.parse('3-5').should.equal(-2);
+      kismet.parse('3*4').should.equal(12);
+      kismet.parse('2+3*4+5').should.equal(19);
+      kismet.parse('4^3').should.equal(64);
+      kismet.parse('16^0.5').should.equal(4);
     });
 });
