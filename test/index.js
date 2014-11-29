1aeeeb80dd65ed0eb8a8749b4c9de0708b7bf7ec
Mara Kim <hacker.root@gmail.com>
2014-11-29 16:28:17 -0600

    Add parentheses test

--- test/index.js
+++ test/index.js
@@ -8,6 +8,7 @@ describe('#parse', function() {
       kismet.parse('3-5').should.equal(-2);
       kismet.parse('3*4').should.equal(12);
       kismet.parse('2+3*4+5').should.equal(19);
+      kismet.parse('(2+3)*4+5').should.equal(25);
       kismet.parse('4^3').should.equal(64);
       kismet.parse('16^0.5').should.equal(4);
     });
