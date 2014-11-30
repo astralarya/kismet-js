26db9800aa506e6b47eec580d0386a917690dd42
Mara Kim <hacker.root@gmail.com>
2014-11-29 19:02:09 -0600

    Add interactive mode

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 
 /* dependencies */
 
-var kismet = require('./dist/kismet').parser;
+var prompt = require('prompt'),
+    kismet = require('./dist/kismet').parser;
+
+prompt.message = '';
+prompt.delimiter = '';
+prompt.colors = false;
 
 
 /* exports */
@@ -16,5 +21,33 @@ module.exports = {
    */
   parse: function(input) {
     return kismet.parse(input);
+  },
+
+  /**
+   * Start an interactive session
+   */
+  prompt: function(callback) {
+    prompt.start();
+    function loop(prev) {
+      prompt.get({
+        properties: {
+          input: {
+            description: '>>>',
+            type: 'string',
+            default: prev
+          }}},
+        function(err, result) {
+          if(result.input !== 'quit') {
+            var result = kismet.parse(result.input)
+            if(callback) {
+              callback(result);
+            } else {
+              console.log(result);
+            }
+            loop(result.input);
+          }
+        });
+    };
+    loop();
   }
 };
---
+++ kismet.js
@@ -0,0 +1,3 @@
+var kismet = require('./index');
+
+kismet.prompt();
